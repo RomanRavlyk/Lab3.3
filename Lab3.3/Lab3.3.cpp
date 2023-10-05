294c93581aff9a067be4ab879e1764209f4750dd
RomanRavlyk <Roman.Ravlyk.Ri.2023@lpnu.ua>
2023-10-05 22:27:03 +0300

    Add project files.

---
+++ Lab3.3/Lab3.3.cpp
@@ -0,0 +1,44 @@
+// Lab_03_3.cpp
+// Равлик Роман
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 23
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double y;
+    double R;
+    double p = atan(1.0) * 4;
+    cout << "x = "; cin >> x;
+    cout << "R = "; cin >> R;
+    //  розгалуження в повній формі
+    
+    if (x <= -7 - R)
+        y = R;
+    else
+        if (x > -7 - R && x <= -7 + R)
+            y = R - pow(R, 2)-pow((x-7), 2);
+        else
+            if (-7 < x <= -7 + R)
+                y = R + sqrt((R * R) - ((x + 7) * (x + 7)));
+            else
+                if (x > -7 + R && x <= -4)
+                    y = R;
+                else
+                    if (x > 0 && x <= p)
+                        y = sin(x);
+                    else
+                        y = x + p;
+    cout << endl;
+    cout << "y = " << y << endl;
+
+    cin.get();
+    return 0;
+
+}
